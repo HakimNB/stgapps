776245067701d56e6b065001d2d745b0be9ea23d
Hakim Hauston <hakim.hauston@gmail.com>
2019-09-14 02:29:45 +0800

    [singtel] sbt new echo-server + setup route and model object

---
+++ apicaller/server/app/controllers/HomeController.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+case class Echo(
+    var data: String
+) {
+  
+}
+
+object Echo {
+  implicit val echoJsonFormat = Json.format[Echo]
+}
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  
+  def appenddate() = Action { implicit request: Request[AnyContent] =>
+    val oReq = request.body.asJson
+    val jsReq = oReq.getOrElse(JsString("null"))
+    val data = (jsReq \ "data").getOrElse(JsString("no data")).as[String]
+    val echo = Echo(data)
+    Ok(Json.toJson(echo).toString())
+  }
+}
