6dd6677a357e5edbd159d3f5f06ed9271011679f
Hakim Hauston <hakim.hauston@gmail.com>
2019-09-14 12:40:12 +0800

    [singtel] apiserver - fulfil call count requirement

--- apicaller/server/app/controllers/HomeController.scala
+++ apicaller/server/app/controllers/HomeController.scala
@@ -1,19 +1,52 @@
 package controllers
 
 import javax.inject._
+import java.util.Date
+import java.util.Calendar
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.Logger
 
 case class Echo(
-    var data: String
+    var data: String,
+    var callcount: Int
 ) {
   
 }
 
 object Echo {
   implicit val echoJsonFormat = Json.format[Echo]
+  
+  // these should be in GlobalSettings, or memcache
+  private var lc: java.util.Date = new Date()
+  private var cc: Int = 0
+  
+  def called() = {
+    if ( !issameday(lc) ) {
+      cc = 0
+    }
+    cc += 1
+  }
+  
+  def getlastcalled() = {
+    lc
+  }
+  
+  def getcallcount() = {
+    cc
+  }
+  
+  def issameday(date: Date) = {
+    val today: Calendar = Calendar.getInstance()
+    val thedate: Calendar = Calendar.getInstance()
+    thedate.setTime(date)
+    val sameday = today.get(Calendar.DAY_OF_MONTH) == thedate.get(Calendar.DAY_OF_MONTH)
+    val samemonth = today.get(Calendar.MONTH) == thedate.get(Calendar.MONTH)
+    val sameyear = today.get(Calendar.YEAR) == thedate.get(Calendar.YEAR)
+    val res = sameday && samemonth && sameyear
+    res
+  }
 }
 
 /**
@@ -34,12 +67,15 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index())
   }
   
-  def appenddate() = Action { implicit request: Request[AnyContent] =>
+  def appendcount() = Action { implicit request: Request[AnyContent] =>
     val oReq = request.body.asJson
     val jsReq = oReq.getOrElse(JsString("null"))
     val data = (jsReq \ "data").getOrElse(JsString("no data")).as[String]
     Logger.debug("data: " + data)
-    val echo = Echo(data)
+    Echo.called()
+    val lastcalled = Echo.getlastcalled()
+    val callcount = Echo.getcallcount()
+    val echo = Echo(data + " " + callcount, callcount)
     Ok(Json.toJson(echo).toString())
   }
 }
